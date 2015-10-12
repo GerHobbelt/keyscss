a933ed64c29dfbe2ffdab269a3cc13aaf95afdd7
Ger Hobbelt <ger@hobbelt.com>
2015-10-12 23:46:17 +0200

    - updated keys.less to match the latest keys.css - added autoprefixer to the build toolchain - compiled the keys.css file from keys.less and verified that it's the same as before (except the bits that autoprefixer did change) - removed the `npm run build` task as it breaks on Windows; use the build.sh script everywhere (on Windows you're supposed to have MSysGit or some similar UNIX-y environment installed)

--- build.sh
+++ build.sh
@@ -1,3 +1,17 @@
 #! /bin/bash
 node_modules/.bin/lessc --verbose keys.less keys.css
+node_modules/.bin/postcss -u autoprefixer -o tmp2.css keys.css
+cat > tmp1.css <<EOF
+/**
+ * KEYS.css
+ *
+ * A simple stylesheet for rendering beautiful keyboard-style elements.
+ *
+ * Author:  Michael Hüneburg
+ * Website: http://michaelhue.com/keyscss
+ * License: MIT License (see LICENSE.txt)
+ */
 
+EOF
+cat tmp1.css tmp2.css > keys.css
+rm tmp1.css tmp2.css
